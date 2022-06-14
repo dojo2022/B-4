9932c59019fbcb8e672ee7bf2e92b1e8d2d25bc6
dojo2022-enomoto <103239217+dojo2022-enomoto@users.noreply.github.com>
2022-06-14 16:55:58 +0900

    Update NewRegisterServlet.java

--- src/src/servlet/NewRegisterServlet.java
+++ src/src/servlet/NewRegisterServlet.java
@@ -1,11 +1,17 @@
 package servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import dao.UserDao;
+import model.Result;
+import model.User;
 
 /**
  * Servlet implementation class NewRegisterServlet
@@ -13,29 +19,44 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/NewRegisterServlet")
 public class NewRegisterServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public NewRegisterServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//新規登録ページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/newregister.jsp");
+		dispatcher.forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+	// リクエストパラメータを取得する
+		request.setCharacterEncoding("UTF-8");
+		String id = request.getParameter("id");
+		String user_id = request.getParameter("user_id");
+		String user_name = request.getParameter("user_name");
+		String address = request.getParameter("address");
+		String image = request.getParameter("image");
+		String comment = request.getParameter("comment");
+		String pw = request.getParameter("pw");
 
+		// 登録処理を行う
+		UserDao iDao = new UserDao();
+		if (iDao.insert(new User(id,user_id,user_name,address,image,comment,pw))) {	// 登録成功
+			request.setAttribute("result",
+			new Result("登録成功！", "レコードを登録しました。", "/dotchiha/LoginServlet"));
+		}
+		else {												// 登録失敗
+			request.setAttribute("result",
+			new Result("登録失敗！", "レコードを登録できませんでした。", "/dotchiha/LoginServlet"));
+		}
+
+		// 結果ページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/idpwregistresult.jsp");
+		dispatcher.forward(request, response);
+
+	}
 }
