938401a160a182cfa1a0f808786ee7493357e7bc
dojo2022-enomoto <103239217+dojo2022-enomoto@users.noreply.github.com>
2022-06-15 17:37:43 +0900

    Update NewPostServlet.java

--- src/src/servlet/NewPostServlet.java
+++ src/src/servlet/NewPostServlet.java
@@ -1,19 +1,31 @@
 package servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+import dao.PostDao;
+import model.Post;
+import model.Result;
+
+/*import dao.PostDao;
+import model.Post;*/
 
 /**
  * Servlet implementation class NewPostServlet
  */
+@MultipartConfig(location = "C:\\dotchiha\\WebContent\\img")
 @WebServlet("/NewPostServlet")
 public class NewPostServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -26,16 +38,51 @@ public class NewPostServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//新規投稿ページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/newpost.jsp");
+		dispatcher.forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+		request.setCharacterEncoding("UTF-8");
+		Part image=request.getPart("image");//getPartで取得
+		//リクエストパラメータの取得
+		String posttitle = request.getParameter("posttitle");
+		String image = this.getFileName(image);
+		String cord = request.getParameter("cord");
+		String postcomment = request.getParameter("postcomment");
+		//画像のアップロード処理
+		request.setAttribute("image", image);
+		image.write(image);
 
+		//登録処理
+		PostDao pDao = new PostDao();
+		if (pDao.insert(new Post(posttitle,image,cord,postcomment))) {	// 登録成功
+			request.setAttribute("result",
+			new Result("登録成功！", "レコードを登録しました。", "/dotchiha/LoginServlet"));
+		}
+		else {												// 登録失敗
+			request.setAttribute("result",
+			new Result("登録失敗！", "レコードを登録できませんでした。", "/dotchiha/LoginServlet"));
+		}
+		//結果ページにフォワード
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/idpwregistresult.jsp");
+		dispatcher.forward(request, response);
+	}
+	//getFileName処理
+	//ファイルの名前を取得する
+	private String getFileName(Part image) {
+		String name=null;
+		for(String dispotion:image.getHeader("Content-Dispotion").split(";")) {
+			if (dispotion.trim().startsWith("filename")) {
+			name = dispotion.substring(dispotion.indexOf("=") + 1).replace("\"", "").trim();
+			name = name.substring(name.lastIndexOf("\\") + 1);
+			break;
+			}
+		}
+		return name;
+	}
 }
