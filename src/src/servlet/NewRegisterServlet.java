aec26bbc1dee6096d7c1705ca758a4585ff869b8
dojo2022-enomoto <103239217+dojo2022-enomoto@users.noreply.github.com>
2022-06-15 17:37:40 +0900

    Update NewRegisterServlet.java

--- src/src/servlet/NewRegisterServlet.java
+++ src/src/servlet/NewRegisterServlet.java
@@ -4,10 +4,12 @@ import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 
 import dao.UserDao;
 import model.Result;
@@ -16,6 +18,9 @@ import model.User;
 /**
  * Servlet implementation class NewRegisterServlet
  */
+//↓これ絶対入れてね！！
+@MultipartConfig(location = "C:\\dotchiha\\WebContent\\icon") // アップロードファイルの一時的な保存先
+
 @WebServlet("/NewRegisterServlet")
 public class NewRegisterServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -33,19 +38,25 @@ public class NewRegisterServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	// リクエストパラメータを取得する
 		request.setCharacterEncoding("UTF-8");
+		Part part=request.getPart("icon");//getPartで取得
+		// リクエストパラメータを取得する
 		String id = request.getParameter("id");
 		String user_id = request.getParameter("user_id");
 		String user_name = request.getParameter("user_name");
 		String address = request.getParameter("address");
-		String image = request.getParameter("image");
-		String comment = request.getParameter("comment");
+		//thisでこのServlet上でgetFileName処理を行う
+		String icon = this.getFileName(part);
+		String freespace = request.getParameter("freespace");
 		String pw = request.getParameter("pw");
+
+		//画像のアップロード処理
+		request.setAttribute("image", icon);
+		part.write(icon);
 
 		// 登録処理を行う
 		UserDao iDao = new UserDao();
-		if (iDao.insert(new User(id,user_id,user_name,address,image,comment,pw))) {	// 登録成功
+		if (iDao.insert(new User(id,user_id,user_name,address,icon,freespace,pw))) {	// 登録成功
 			request.setAttribute("result",
 			new Result("登録成功！", "レコードを登録しました。", "/dotchiha/LoginServlet"));
 		}
@@ -57,6 +68,18 @@ public class NewRegisterServlet extends HttpServlet {
 		// 結果ページにフォワードする
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/idpwregistresult.jsp");
 		dispatcher.forward(request, response);
-
+	}
+	//getFileName処理
+	//ファイルの名前を取得する
+	private String getFileName(Part part) {
+		String name=null;
+		for(String dispotion:part.getHeader("Content-Dispotion").split(";")) {
+			if (dispotion.trim().startsWith("filename")) {
+			name = dispotion.substring(dispotion.indexOf("=") + 1).replace("\"", "").trim();
+			name = name.substring(name.lastIndexOf("\\") + 1);
+			break;
+			}
+		}
+		return name;
 	}
 }
