20fe29af6adecd5a3c9b6b2a8b6d0256e7b16543
dojo2022-enomoto <103239217+dojo2022-enomoto@users.noreply.github.com>
2022-06-16 11:56:38 +0900

    Update NewPostServlet.java

--- src/src/servlet/NewPostServlet.java
+++ src/src/servlet/NewPostServlet.java
@@ -15,8 +15,6 @@ import dao.PostDao;
 import model.Post;
 import model.Result;
 
-/*import dao.PostDao;
-import model.Post;*/
 
 /**
  * Servlet implementation class NewPostServlet
@@ -51,25 +49,25 @@ public class NewPostServlet extends HttpServlet {
 		Part image=request.getPart("image");//getPartで取得
 		//リクエストパラメータの取得
 		String posttitle = request.getParameter("posttitle");
-		String image = this.getFileName(image);
+		String image_S = this.getFileName(image);
 		String cord = request.getParameter("cord");
 		String postcomment = request.getParameter("postcomment");
 		//画像のアップロード処理
 		request.setAttribute("image", image);
-		image.write(image);
+		image.write(image_S);
 
 		//登録処理
 		PostDao pDao = new PostDao();
-		if (pDao.insert(new Post(posttitle,image,cord,postcomment))) {	// 登録成功
+		if (pDao.insert(new Post(posttitle,image_S,cord,postcomment))) {	// 登録成功
 			request.setAttribute("result",
-			new Result("登録成功！", "レコードを登録しました。", "/dotchiha/LoginServlet"));
+			new Result("登録成功！", "レコードを登録しました。", "/dotchiha/NewPostServlet"));
 		}
 		else {												// 登録失敗
 			request.setAttribute("result",
-			new Result("登録失敗！", "レコードを登録できませんでした。", "/dotchiha/LoginServlet"));
+			new Result("登録失敗！", "レコードを登録できませんでした。", "/dotchiha/NewPostServlet"));
 		}
 		//結果ページにフォワード
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/idpwregistresult.jsp");
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/result.jsp");
 		dispatcher.forward(request, response);
 	}
 	//getFileName処理
