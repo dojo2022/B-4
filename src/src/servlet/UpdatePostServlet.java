5df4513d9ef4aced6627b984847c13e36765c714
dojo2022-enomoto <103239217+dojo2022-enomoto@users.noreply.github.com>
2022-06-20 13:04:29 +0900

    Update UpdatePostServlet.java

--- src/src/servlet/UpdatePostServlet.java
+++ src/src/servlet/UpdatePostServlet.java
@@ -2,12 +2,15 @@ package servlet;
 
 import java.io.IOException;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import dao.PostDao;
+import model.Post;
+import model.Result;
 
 /**
  * Servlet implementation class UpdatePostServlet
@@ -16,28 +19,50 @@ import javax.servlet.http.HttpServletResponse;
 public class UpdatePostServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public UpdatePostServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+/*	/**
+	* @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	*//*
+		protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//投稿更新ページにフォワードする
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/updatepost.jsp");
 		dispatcher.forward(request, response);
-	}
+		}
+*/
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		//リクエストパラメータを取得する
+		String id = request.getParameter("id");
+		String posttitle = request.getParameter("posttitle");
+		String image = request.getParameter("image");
+		String cord = request.getParameter("cord");
+		String postcomment = request.getParameter("postcomment");
 
+		//更新・削除を行う
+		PostDao pDao = new PostDao();
+		if (request.getParameter("SUBMIT").equals("更新")) {
+			if (pDao.update(new Post("","",posttitle,image,cord,postcomment,""))) {	// 更新成功
+				request.setAttribute("result",
+				new Result("更新成功！", "レコードを更新しました。", "/simpleBC/MenuServlet"));
+			}
+			else {												// 更新失敗
+				request.setAttribute("result",
+				new Result("更新失敗！", "レコードを更新できませんでした。", "/simpleBC/MenuServlet"));
+			}
+		}
+		else {
+			if (pDao.delete(id)) {	// 削除成功
+				request.setAttribute("result",
+				new Result("削除成功！", "レコードを削除しました。", "/simpleBC/MenuServlet"));
+			}
+			else {						// 削除失敗
+				request.setAttribute("result",
+				new Result("削除失敗！", "レコードを削除できませんでした。", "/simpleBC/MenuServlet"));
+			}
+		}
 	}
-
 }
